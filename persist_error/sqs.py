79950b459b2dadf6af24df7b382a8d4c8c0d7a26
Andrew Yan <ayan@usgs.gov>
2020-02-21 16:15:18 -0600

    add deduplication id for FIFO queues

--- persist_error/sqs.py
+++ persist_error/sqs.py
@@ -2,15 +2,18 @@
 Module containing functions for persisting inputs that cause failures in for a particular state.
 
 """
+import uuid
+
 import boto3
 
 
 def send_message(queue_url, message_body, message_grp_id='step_function_error', region='us-west-2'):
     """
-    Send a message to an SQS queue.
+    Send a message to an SQS FIFO queue.
 
     :param str queue_url: http url of the SQS queue
     :param str message_body: the message body
+    :param str dedup_id: string use for deduplication of messages in the SQS FIFO queue
     :param str message_grp_id: specifies that message belongs in a group in the FIFO queue
     :param str region: AWS region
     :return: message send response
@@ -18,5 +21,13 @@ def send_message(queue_url, message_body, message_grp_id='step_function_error', 
 
     """
     sqs = boto3.client('sqs', region_name=region)
-    resp = sqs.send_message(QueueUrl=queue_url, MessageBody=message_body, MessageGroupId=message_grp_id)
+
+    deduplication_id = str(uuid.uuid4())
+
+    resp = sqs.send_message(
+        QueueUrl=queue_url,
+        MessageBody=message_body,
+        MessageDeduplicationId=deduplication_id,
+        MessageGroupId=message_grp_id
+    )
     return resp
--- persist_error/tests/test_sqs.py
+++ persist_error/tests/test_sqs.py
@@ -13,12 +13,16 @@ class TestSendMessage(TestCase):
         self.queue_url = 'https://sqs.us-south-10.amazonaws.com/887501/some-queue-name'
         self.message_body = 'a message body'
         self.message_grp_id = 'message_id'
+        self.deduplication_id = '584-2ab-3381z'
         self.region = 'us-south-10'
 
+    @mock.patch('persist_error.sqs.uuid')
     @mock.patch('persist_error.sqs.boto3.client', autospec=True)
-    def test_message_send(self, m_client):
+    def test_message_send(self, m_client, m_uuid):
         mock_sqs = mock.Mock()
         m_client.return_value = mock_sqs
+
+        m_uuid.uuid4.return_value = self.deduplication_id
 
         send_message(
             queue_url=self.queue_url,
@@ -30,5 +34,6 @@ class TestSendMessage(TestCase):
         mock_sqs.send_message.assert_called_with(
             QueueUrl=self.queue_url,
             MessageBody=self.message_body,
+            MessageDeduplicationId=self.deduplication_id,
             MessageGroupId=self.message_grp_id
         )
