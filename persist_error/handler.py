20e5ddd80de7a696eacd15ff1f9533e16dd15aad
Andrew Yan <ayan@usgs.gov>
2020-02-24 10:39:44 -0600

    get the execution history of the run

--- persist_error/handler.py
+++ persist_error/handler.py
@@ -14,9 +14,15 @@ def lambda_handler(event, context):
     logger.info(f'Event: {event}')
     logger.info(f'Context: {context}')
 
+    execution_arn = event['executionArn']
     queue_url = os.getenv('AWS_SQS_QUEUE_URL')
     region = os.getenv('AWS_DEPLOYMENT_REGION')
 
+    exec_history = get_execution_history(
+        execution_arn=execution_arn,
+        region=region
+    )
+    logger.info(f'Execution History: {exec_history}')
     send_message(
         queue_url=queue_url,
         message_body=json.dumps(event),
