88c6cb2d360b4454d5f7526cb31495e36080b08a
Andrew Yan <ayan@usgs.gov>
2020-02-24 12:49:53 -0600

    return root input

--- persist_error/sfn.py
+++ persist_error/sfn.py
@@ -23,7 +23,6 @@ def find_root_failure_state(execution_history):
         search_result = search_dictionary_list(execution_events, 'id', state_previous_event_id)[0]
         state = search_result
     state_event_details = state['stateEnteredEventDetails']
-    return {
-        'state': state_event_details['name'],
-        'input': ast.literal_eval(state_event_details['input'])
-    }
+    root_input = ast.literal_eval(state_event_details['input'])
+    root_input['resumeState'] = state_event_details['name']
+    return root_input
