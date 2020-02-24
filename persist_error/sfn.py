b7e3c3f92d5249d57e14129c246aba8f0bf42785
Andrew Yan <ayan@usgs.gov>
2020-02-24 10:48:45 -0600

    correct service name

--- persist_error/sfn.py
+++ persist_error/sfn.py
@@ -6,6 +6,6 @@ import boto3
 
 
 def get_execution_history(execution_arn, region='us-west-2'):
-    sfn = boto3.client('sfn', region_name=region)
+    sfn = boto3.client('stepfunctions', region_name=region)
     history = sfn.get_execution_history(executionArn=execution_arn)
     return history
