6626c05e2b9b5141f1feed6a05f9511c4d8c1ac3
Andrew Yan <ayan@usgs.gov>
2020-04-08 16:00:01 -0500

    handle AWS API ClientError execptions

--- persist_error/handler.py
+++ persist_error/handler.py
@@ -2,7 +2,9 @@ import json
 import os
 import logging
 
-from .sfn import get_execution_history, get_state_machine_input, find_root_failure_state
+from botocore.exceptions import ClientError
+
+from .sfn import get_execution_history, find_root_failure_state
 from .sns import send_notification
 from .sqs import send_message
 
@@ -21,14 +23,29 @@ def lambda_handler(event, context):
     max_retries = int(os.getenv('MAX_RETRIES', 4))
 
     execution_arn = event['executionArn']
-    exec_history = get_execution_history(
-        execution_arn=execution_arn,
-        region=region
-    )
-    logger.info(f'Execution History: {exec_history}')
+    initial_input = event['startInput']
+    logger.info(f'Initial input: {initial_input}')
 
+    # handle API throttling by sending the initial input back to the queue
     try:
-        initial_input = get_state_machine_input(exec_history)
+        exec_history = get_execution_history(
+            execution_arn=execution_arn,
+            region=region
+        )
+    except ClientError:
+        send_message(
+            queue_url=queue_url,
+            message_body=json.dumps(initial_input),
+            region=region
+        )
+        logger.info(f'API throttled, event sent to {queue_url}.')
+        return initial_input
+    else:
+        logger.info(f'Execution History: {exec_history}')
+
+    # figure out which state failed
+    # if it can't be figured out, send a notification for a human to help
+    try:
         failure_state = find_root_failure_state(exec_history)
     except Exception as e:
         logger.info(f'State parsing error: {repr(e)}', exc_info=True)
@@ -39,9 +56,10 @@ def lambda_handler(event, context):
         send_notification(sns_arn, error_handler_msg)
         return  # drop out of the function
     else:
-        logger.info(f'Initial input: {initial_input}')
         logger.info(f'Failure state: {failure_state}')
 
+    # increment the number failures the data has caused
+    # reset the count if we failed on a different state than before
     try:
         if initial_input['resumeState'] == failure_state['resumeState']:
             failure_state['stepFunctionFails'] += 1  # increment number of failures
@@ -50,6 +68,7 @@ def lambda_handler(event, context):
     except KeyError:
         failure_state['stepFunctionFails'] = 1  # start incrementing failures if this is first one
 
+    # keep a list of previously failed ARNs
     try:
         failure_state['previousExecutions'].append(execution_arn)
     except KeyError:
